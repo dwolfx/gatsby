21a18a4959e469c0774809006ba7af321807f02e
Victor 'dWolf' Morais <victor9009@gmail.com>
2020-08-31 15:36:48 -0300

    clean all stuffs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blog Victor`,
+    description: `Blog pessoal do Victor`,
+    author: `@victor9007`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
